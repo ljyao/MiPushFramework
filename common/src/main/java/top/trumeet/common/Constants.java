69c15177dfcc1ad8bcf6986a1ff5f7987d498368
zts1993 <zts1993@gmail.com>
2018-10-05 09:27:27 +0800

    feat(xposed): update FAKE_CONFIGURATION_PATH

--- common/src/main/java/top/trumeet/common/Constants.java
+++ common/src/main/java/top/trumeet/common/Constants.java
@@ -116,6 +116,6 @@ public final class Constants {
         public static final String GET_VERSION = "top.trumeet.mipush.permissions.GET_VERSION";
     }
 
-    public static final String FAKE_CONFIGURATION_PATH = "/data/riru/modules/mipush_fake/";
+    public static final String FAKE_CONFIGURATION_PATH = "/data/misc/riru/modules/mipush_fake/packages/";
     public static final String FAKE_CONFIGURATION_NAME_TEMPLATE = FAKE_CONFIGURATION_PATH + "%1$s.%2$s";
 }
